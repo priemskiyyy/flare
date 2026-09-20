0c3d4b82dc2924b90dedc7a7b9e359c07df2bf15
yevhenii <yevheniipryiemskyi@gmail.com>
2026-09-21 00:22:48 +0200

    feat(trace): package the breadcrumb bridge

---
+++ packages/trace/src/index.ts
@@ -0,0 +1,8 @@
+export { traceBreadcrumbs } from "src/traceBreadcrumbs";
+
+export type { BreadcrumbTarget } from "src/types/BreadcrumbTarget";
+export type { TraceBreadcrumb } from "src/types/TraceBreadcrumb";
+export type { TraceBreadcrumbMap } from "src/types/TraceBreadcrumbMap";
+export type { TraceBreadcrumbsOptions } from "src/types/TraceBreadcrumbsOptions";
+export type { TraceEvent } from "src/types/TraceEvent";
+export type { TraceEventSource } from "src/types/TraceEventSource";
