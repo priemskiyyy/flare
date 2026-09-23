df0bbce8c47e9f7b218be2c5260f31fbd627ca38
yevhenii <yevheniipryiemskyi@gmail.com>
2026-09-23 16:58:59 +0200

    refactor(bugsnag)!: serve React Native through the one factory

--- packages/adapters/bugsnag/src/reactNative.ts
+++
@@ -1,7 +0,0 @@
-export { bugsnag } from "src/bugsnagReactNative";
-
-export type { BugsnagBreadcrumbConstructorLike } from "src/types/BugsnagBreadcrumbConstructorLike";
-export type { BugsnagBreadcrumbLike } from "src/types/BugsnagBreadcrumbLike";
-export type { BugsnagEventLike } from "src/types/BugsnagEventLike";
-export type { BugsnagLike } from "src/types/BugsnagLike";
-export type { BugsnagReporterOptions } from "src/types/BugsnagReporterOptions";
