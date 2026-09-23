daadd286fe9b30c6e89a1f00d2216244746c3614
yevhenii <yevheniipryiemskyi@gmail.com>
2026-09-23 17:00:34 +0200

    feat(solid): throw FlareError when used outside a provider

--- packages/solid/src/primitives/internal/useFlareContext.ts
+++ packages/solid/src/primitives/internal/useFlareContext.ts
@@ -1,3 +1,4 @@
+import { FlareError } from "@priemskiyyy/flare";
 import { useContext } from "solid-js";
 
 import { FlareContext } from "src/context/FlareContext";
@@ -6,7 +7,10 @@ export const useFlareContext = (subject: string) => {
   const context = useContext(FlareContext);
 
   if (context === undefined) {
-    throw new Error(`${subject} must be used within a FlareProvider.`);
+    throw new FlareError({
+      code: "INVALID_CONFIGURATION",
+      message: `${subject} must be used within a FlareProvider.`,
+    });
   }
 
   return context;
